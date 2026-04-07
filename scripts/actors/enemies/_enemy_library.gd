c97d939f9f6d47b6745dda195b2e8d2b882eb349
Ken <beinion.gamedesigner@outlook.com>
2026-04-07 15:09:04 -0700

    Level

--- scripts/actors/enemies/_enemy_library.gd
+++ scripts/actors/enemies/_enemy_library.gd
@@ -3,10 +3,12 @@ class_name EnemyLibrary
 
 enum EnemyTypes {
 	SLIME,
+	BUBBLE,
 	BOSS_SLIME
 }
 
 const ENEMY_PATHS: Dictionary = {
 	EnemyTypes.SLIME: "res://scenes/actors/enemies/slime.tscn",
+	EnemyTypes.BUBBLE: "res://scenes/actors/enemies/slime.tscn",
 	EnemyTypes.BOSS_SLIME: "res://scenes/actors/enemies/boss_slime.tscn",
 }
